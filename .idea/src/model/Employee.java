c4b3a0d1e9e959d2199ad0d5ac2e78f3503f7878
Ignacio DM <i.dalmaumiravitlles@gmail.com>
2025-06-27 22:06:35 +0200

    employee

--- .idea/src/model/Employee.java
+++ .idea/src/model/Employee.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Employee {
+    int id;
+    String name;
+    String jutje;
 
+    public Employee(int id, String name, String jutje) {
+        this.id = id;
+        this.name = name;
+        this.jutje = jutje;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
 }
