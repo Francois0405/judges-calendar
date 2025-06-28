053adf88cfceecc1aacf5910ea195fb8b1139c3c
Ignacio DM <i.dalmaumiravitlles@gmail.com>
2025-06-28 11:17:45 +0200

    employee

--- .idea/src/model/Employee.java
+++ .idea/src/model/Employee.java
@@ -1,22 +1,55 @@
 package model;
 
 public class Employee {
-    int id;
     String name;
-    String jutje;
+    int numbJutj;
+    int workingPts;
 
-    public Employee(int id, String name, String jutje) {
-        this.id = id;
+    public Employee(String name, int numbJutj, int workingPts) {
         this.name = name;
-        this.jutje = jutje;
+        this.numbJutj = numbJutj;
+        this.workingPts = workingPts;
     }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+
     public String getName() {
         return name;
+    }
+
+    public int getNumbJutj() {
+        return numbJutj;
+    }
+
+    public int getWorkingPts() {
+        return workingPts;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumbJutj(int numbJutj) {
+        this.numbJutj = numbJutj;
+    }
+
+    public void setWorkingPts(int workingPts) {
+        this.workingPts = workingPts;
+    }
+
+    public void holiday(int dia){
+        setWorkingPts(getWorkingPts()-1);
+        //Day.marcarDiaDeFiesta()
+    }
+    public void workingDay(int dia){
+        setWorkingPts(getWorkingPts()+1);
+        //Day.marcarDiaDeTrabajo
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", numbJutj=" + numbJutj +
+                ", workingPts=" + workingPts +
+                '}';
     }
 }
